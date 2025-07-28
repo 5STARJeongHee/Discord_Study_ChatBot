f25d9c48bf95571c68325da13686afcdc1543b4a
JJEONG <46335198+5STARJeongHee@users.noreply.github.com>
2025-07-29 00:11:59 +0900

    feature: help 버튼이 실제 기능이 아닌 커맨드 안내 및 사용법 안내로 변경됨

--- views/help_view.py
+++ views/help_view.py
@@ -1,29 +1,30 @@
 from discord import ui, Interaction, ButtonStyle
-from handlers.goal_handler import handle_register_goal, handle_edit_goal, handle_view_goals, handle_delete_goal, handle_start_goal, handle_stop_goal
+from handlers.help_handler import handle_register_goal, handle_edit_goal, handle_view_goals, handle_delete_goal, handle_start_goal, handle_stop_goal
 
 class HelpButtonView(ui.View):
+    
     # discord, ui view를 상속하여 
     # 데코레이터 방식으로 ui 아이템들을 등록
-    @ui.button(label="목표 등록", custom_id="register_goal", style=ButtonStyle.green)
+    @ui.button(label="목표 등록", custom_id="register_goal", style=ButtonStyle.green, row=0)
     async def register_goal(self, interaction: Interaction, button: ui.Button):
         await handle_register_goal(interaction, button)
 
-    @ui.button(label="목표 수정", custom_id="edit_goal", style=ButtonStyle.blurple)
+    @ui.button(label="목표 수정", custom_id="edit_goal", style=ButtonStyle.blurple, row=0)
     async def edit_goal(self, interaction: Interaction, button: ui.Button):
         await handle_edit_goal(interaction, button)
 
-    @ui.button(label="목표 목록 조회", custom_id="view_goal", style=ButtonStyle.gray)
+    @ui.button(label="목표 목록 조회", custom_id="view_goal", style=ButtonStyle.gray, row=2)
     async def view_goal(self, interaction: Interaction, button: ui.Button):
         await handle_view_goals(interaction, button)
 
-    @ui.button(label="목표 삭제", custom_id="delete_goal", style=ButtonStyle.red)
+    @ui.button(label="목표 삭제", custom_id="delete_goal", style=ButtonStyle.red, row=2)
     async def delete_goal(self, interaction: Interaction, button: ui.Button):
         await handle_delete_goal(interaction, button)
 
-    @ui.button(label="학습 시작", custom_id="start_goal", style=ButtonStyle.success)
+    @ui.button(label="학습 시작", custom_id="start_goal", style=ButtonStyle.success, row=3)
     async def start_goal(self, interaction: Interaction, button: ui.Button):
         await handle_start_goal(interaction, button)
 
-    @ui.button(label="학습 종료", custom_id="stop_goal", style=ButtonStyle.danger)
+    @ui.button(label="학습 종료", custom_id="stop_goal", style=ButtonStyle.danger, row=3)
     async def stop_goal(self, interaction: Interaction, button: ui.Button):
         await handle_stop_goal(interaction, button)
